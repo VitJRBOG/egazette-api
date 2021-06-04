5b71559b7bb69b4efa94a84c05314443fe8357d7
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-06-04 15:22:22 +0500

    Done: Package for working with database

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module bitbucket.org/VitJRBOG/rss_maker
 
 go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+)
---
+++ internal/db/db.go
@@ -0,0 +1,163 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"runtime/debug"
+	"strings"
+
+	config "bitbucket.org/VitJRBOG/rss_maker/internal/config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect(connectionData config.DBConn) (*sql.DB, error) {
+	c := fmt.Sprintf("%s:%s@tcp(%s)/%s",
+		connectionData.Login, connectionData.Password,
+		connectionData.Address, connectionData.DBName)
+	db, err := sql.Open("mysql", c)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+type Feed struct {
+	ID         int
+	SourceName string
+	URL        string
+}
+
+func (f *Feed) SelectFrom(dbase *sql.DB) ([]Feed, error) {
+	query := "SELECT * FROM feed"
+
+	var values []interface{}
+
+	if f.ID != 0 {
+		query += " WHERE id = ?"
+		values = append(values, f.ID)
+	}
+
+	if f.SourceName != "" {
+		if strings.Contains(query, "WHERE") {
+			query += "AND source_name = ?"
+		} else {
+			query += " WHERE source_name = ?"
+		}
+		values = append(values, f.SourceName)
+	}
+
+	if f.URL != "" {
+		if strings.Contains(query, "WHERE") {
+			query += "AND url = ?"
+		} else {
+			query += " WHERE url = ?"
+		}
+		values = append(values, f.URL)
+	}
+
+	rows, err := dbase.Query(query, values...)
+	if err != nil {
+		return []Feed{}, err
+	}
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			log.Printf("%s\n%s\n", err, debug.Stack())
+		}
+	}()
+
+	var feeds []Feed
+
+	for rows.Next() {
+		var feed Feed
+
+		if err := rows.Scan(&feed.ID, &feed.SourceName, &feed.URL); err != nil {
+			return []Feed{}, err
+		}
+
+		feeds = append(feeds, feed)
+	}
+
+	if err := rows.Err(); err != nil {
+		return []Feed{}, err
+	}
+
+	return feeds, nil
+}
+
+type VKAccess struct {
+	ID          int
+	FeedID      int
+	AccessToken string
+	VKID        int
+}
+
+func (v *VKAccess) SelectFrom(dbase *sql.DB) ([]VKAccess, error) {
+	query := "SELECT * FROM vk_access"
+
+	var values []interface{}
+
+	if v.ID != 0 {
+		query += " WHERE id = ?"
+		values = append(values, v.ID)
+	}
+
+	if v.FeedID != 0 {
+		if strings.Contains(query, "WHERE") {
+			query += "AND feed_id = ?"
+		} else {
+			query += " WHERE feed_id = ?"
+		}
+		values = append(values, v.FeedID)
+	}
+
+	if v.AccessToken != "" {
+		if strings.Contains(query, "WHERE") {
+			query += "AND access_token = ?"
+		} else {
+			query += " WHERE access_token = ?"
+		}
+		values = append(values, v.AccessToken)
+	}
+
+	if v.VKID != 0 {
+		if strings.Contains(query, "WHERE") {
+			query += "AND vk_id = ?"
+		} else {
+			query += " WHERE vk_id = ?"
+		}
+		values = append(values, v.VKID)
+	}
+
+	rows, err := dbase.Query(query, values...)
+	if err != nil {
+		return []VKAccess{}, err
+	}
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			log.Printf("%s\n%s\n", err, debug.Stack())
+		}
+	}()
+
+	var vkAccesses []VKAccess
+
+	for rows.Next() {
+		var vkAccess VKAccess
+
+		if err := rows.Scan(&vkAccess.ID, &vkAccess.FeedID,
+			&vkAccess.AccessToken, &vkAccess.VKID); err != nil {
+			return []VKAccess{}, err
+		}
+
+		vkAccesses = append(vkAccesses, vkAccess)
+	}
+
+	if err := rows.Err(); err != nil {
+		return []VKAccess{}, err
+	}
+
+	return vkAccesses, nil
+}
