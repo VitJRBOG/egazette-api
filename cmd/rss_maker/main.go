9483901242da9a4094e36550a8e1a239ac1cfd9e
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-06-04 15:29:13 +0500

    Done: File main.go prepared for running of program

--- cmd/rss_maker/main.go
+++ cmd/rss_maker/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"runtime/debug"
+
+	config "bitbucket.org/VitJRBOG/rss_maker/internal/config"
+	server "bitbucket.org/VitJRBOG/rss_maker/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	dbConn, err := config.GetDBConnectionData()
+	if err != nil {
+		log.Fatalf("%s\n%s\n", err.Error(), debug.Stack())
+	}
+
+	serverCfg, err := config.GetServerConfig()
+	if err != nil {
+		log.Fatalf("%s\n%s\n", err.Error(), debug.Stack())
+	}
+
+	server.Start(dbConn, serverCfg)
 }
