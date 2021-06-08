790f516192314c88539a07695cb13099cb5a4bdf
Vitaly Bogryakov <VitJRBOG@users.noreply.github.com>
2021-06-08 14:37:48 +0500

    Refactor: Decomposition of function ComposeRSS()
    Алгоритм, собирающий информацию о сообществе, вынесен в отдельную функцию parseCommunityInfo().
    
    Алгоритм, собирающий информацию о публикациях, вынесен в отдельную функцию parseWallPosts().
    
    Алгоритм, собирающий информацию о прикрепленных к публикации фотографиях, вынесен в отдельную функцию parseWallPostAttachments().

--- internal/vk/collector/collector.go
+++ internal/vk/collector/collector.go
@@ -12,10 +12,25 @@ import (
 func ComposeRSS(community vkapi.Community, wallPosts []vkapi.WallPost) (rss.RSS, error) {
 	var r rss.RSS
 
+	r = parseCommunityInfo(r, community)
+
+	r, err := parseWallPosts(r, wallPosts)
+	if err != nil {
+		return rss.RSS{}, err
+	}
+
+	return r, nil
+}
+
+func parseCommunityInfo(r rss.RSS, community vkapi.Community) rss.RSS {
 	r.Channel.Title = community.Name
 	r.Channel.Link = "https://vk.com/" + community.ScreenName
 	r.Channel.Description = strings.ReplaceAll(community.Description, "\n", "<br>")
 
+	return r
+}
+
+func parseWallPosts(r rss.RSS, wallPosts []vkapi.WallPost) (rss.RSS, error) {
 	for _, wallPost := range wallPosts {
 		var rssItem rss.Item
 
@@ -27,22 +42,29 @@ func ComposeRSS(community vkapi.Community, wallPosts []vkapi.WallPost) (rss.RSS,
 		if err != nil {
 			return rss.RSS{}, err
 		}
-		rssItem.Link = fmt.Sprintf("https://vk.com/wall%d_%d", wallPost.OwnerID, wallPost.ID)
-		if len(wallPost.Attachments) > 0 {
-			for _, attachment := range wallPost.Attachments {
-				if attachment.Type != "photo" {
-					continue
-				}
 
-				rssItem.Description = fmt.Sprintf("<img src=\"%s\">\n%s",
-					getMaxSizePhotoURL(attachment.Photo), rssItem.Description)
-			}
-		}
+		rssItem.Link = fmt.Sprintf("https://vk.com/wall%d_%d", wallPost.OwnerID, wallPost.ID)
+		rssItem = parseWallPostAttachments(rssItem, wallPost)
 
 		r.Channel.Items = append(r.Channel.Items, rssItem)
 	}
 
 	return r, nil
+}
+
+func parseWallPostAttachments(rssItem rss.Item, wallPost vkapi.WallPost) rss.Item {
+	if len(wallPost.Attachments) > 0 {
+		for _, attachment := range wallPost.Attachments {
+			if attachment.Type != "photo" {
+				continue
+			}
+
+			rssItem.Description = fmt.Sprintf("<img src=\"%s\">\n%s",
+				getMaxSizePhotoURL(attachment.Photo), rssItem.Description)
+		}
+	}
+
+	return rssItem
 }
 
 func getWallPostTitle(text string) string {
