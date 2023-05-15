18a7bb00dec54c0e2dc204c217b417745d773584
Vitaly Bogryakov <vitjrbog@gmail.com>
2023-05-15 09:24:03 +0500

    Described the fundamental logger for the server

--- internal/server/launcher.go
+++ internal/server/launcher.go
@@ -4,14 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 // Up starts the server.
 func Up() {
-	address := fmt.Sprintf(":%d", 8080)
-	err := http.ListenAndServe(address, nil)
+	// TODO: call the requests handling
+
+	address := fmt.Sprintf(":%d", 8080) // FIXME: remove hardcoded port
+	err := http.ListenAndServe(address, logging(http.DefaultServeMux))
 
 	if err != nil {
 		log.Fatalf("server launch error: %s", err)
 	}
+}
+
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		begins := time.Now()
+		next.ServeHTTP(w, r)
+		timeElapsed := time.Since(begins)
+
+		log.Printf("[%s] %s %s", r.Method, r.RequestURI, timeElapsed)
+	})
 }
