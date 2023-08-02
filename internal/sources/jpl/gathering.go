d6f35bc8d65307ce73c53c3d16ea1724145f79ad
Vitaly Bogryakov <vitjrbog@gmail.com>
2023-08-02 09:37:24 +0500

    Bugfix: the JPL gatherer fall down because of a different format date

--- internal/sources/jpl/gathering.go
+++ internal/sources/jpl/gathering.go
@@ -92,6 +92,15 @@ func extractDate(tag *html.Node) (string, string) {
 	dateLayout := "January 2, 2006 -0700"
 	date := fmt.Sprintf("%s +0000", tag.Data)
 
+	switch {
+	case strings.Contains(date, "Aug.") || strings.Contains(date, "Oct.") ||
+		strings.Contains(date, "Nov.") || strings.Contains(date, "Dec.") ||
+		strings.Contains(date, "Jan.") || strings.Contains(date, "Feb."):
+		dateLayout = "Jan. 2, 2006 -0700"
+	case strings.Contains(date, "Sept."):
+		dateLayout = "Janu. 2, 2006 -0700"
+	}
+
 	return dateLayout, date
 }
 
